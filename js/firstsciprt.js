6bfedfd5449dddb3771dba6b0c37b7d11e87e725
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2021-11-26 20:28:55 +0200

    scriptic))

---
+++ js/firstsciprt.js
@@ -0,0 +1,22 @@
+const btnAbb = document.querySelector('button[data-Abb]');
+const resetBtn = document.querySelector('button[data-reset]')
+const valueInput = document.querySelector('input[data-value]');
+const outputEl = document.querySelector('.js-output span');
+ 
+let total = 0;
+
+btnAbb.addEventListener('click', function () {
+    console.log('click hello');
+
+    const value = Number(valueInput.value);
+
+    total += value;
+    outputEl.textContent = total;
+    valueInput.value = '';
+})
+
+resetBtn.addEventListener('click', function () {
+    total = 0;
+    outputEl.textContent = total;
+
+})
